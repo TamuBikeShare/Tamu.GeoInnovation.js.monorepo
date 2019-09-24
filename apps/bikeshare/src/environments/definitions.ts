ae4484b502f5fc3da26461989c8db60cf3989ba0
jacobericson <jakematt2004@yahoo.com>
2019-09-23 19:58:04 -0500

    Missed a couple things

--- apps/bikeshare/src/environments/definitions.ts
+++ apps/bikeshare/src/environments/definitions.ts
@@ -1,8 +1,6 @@
 export const Connections = {
   basemapUrl: 'https://gis.tamu.edu/arcgis/rest/services/FCOR/BaseMap_20190813/MapServer',
   inforUrl: 'https://gis.tamu.edu/arcgis/rest/services/FCOR/MapInfo_20190529/MapServer',
-  accessibleUrl: 'https://fc-gis.tamu.edu/arcgis/rest/services/FCOR/ADA_120717/MapServer/0',
-  constructionUrl: 'https://gis.tamu.edu/arcgis/rest/services/FCOR/Construction_2018/MapServer',
   departmentUrl: 'https://fc-gis.tamu.edu/arcgis/rest/services/FCOR/DepartmentSearch/MapServer/1',
   tsMainUrl: 'https://gis.tamu.edu/arcgis/rest/services/TS/TS_Main/MapServer',
   bikeRacksUrl: 'https://gis.tamu.edu/arcgis/rest/services/TS/TS_Bicycles/MapServer/3',
@@ -15,13 +13,6 @@ export const Definitions = {
     name: 'Buildings',
     url: `${Connections.basemapUrl}/1`,
     popupComponent: 'BuildingPopupComponent'
-  },
-  CONSTRUCTION: {
-    id: 'construction_zone',
-    layerId: 'construction_zone-layer',
-    name: 'Construction Zone',
-    url: `${Connections.constructionUrl}`,
-    popupComponent: 'ConstructionPopupComponent'
   },
   TRANSPORTATION_PARKING: {
     id: 'transportation-parking',
